8b92f313b64098739b73a0b685b352f88107a391
tungbeoii <hamuda1977pun0suy@hotmail.com>
2024-05-11 16:14:18 +0700

    test task

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-function add(x, y) {
-  return x + y;
+function serialize(root) {
+  const result = [];
+  const queue = [root];
+  while (queue.length) {
+    const node = queue.shift();
+    if (node) {
+      result.push(node.val);
+      queue.push(node.left, node.right);
+    } else {
+      result.push(null);
+    }
+  }
+  while (result[result.length - 1] === null) {
+    result.pop();
+  }
+  return result;
 }
-
-const result = add(5, 3);
-console.log(result); // 8
