a9a3e07f73652f1e8478d12964d7a4d84a92a2cf
tungbeoii <hamuda1977pun0suy@hotmail.com>
2024-05-14 13:36:35 +0700

    update sdk commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const uniqueValues = (arr) => [...new Set(arr)];
+function findPeakElement(nums) {
+  let left = 0;
+  let right = nums.length - 1;
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] < nums[mid + 1]) left = mid + 1;
+    else right = mid;
+  }
+  return left;
+}
